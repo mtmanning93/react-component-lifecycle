b4b2e6d5e0ea5c2b2ce0db4bd79196c945b564a8
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-05 04:49:07 +0000

    Create controlled from file and link to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import LifeCyclesCDM from "./components/LifeCyclesCDM";
-import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import ControlledForm from "./components/ControlledForm";
+// import LifeCyclesCDM from "./components/LifeCyclesCDM";
+// import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
   return (
     <div className="App">
       {/* <LifeCyclesCDM /> */}
-      <LifeCyclesCDU />
+      {/* <LifeCyclesCDU /> */}
+      {/* <LifeCyclesCWU /> */}
+      <ControlledForm />
     </div>
   )
 }
