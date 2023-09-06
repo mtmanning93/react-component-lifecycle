fc7b681cdaa8efc822c5c30c9065736c0a0db07a
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-06 10:50:50 +0000

    create use state with objects file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
+import UseStateWithObjects from "./components/UseStateWithObjects";
 // import ControlledFormHooks from "./components/ControlledFormHooks";
-import UseStateWithArray from "./components/UseStateWithArray";
+// import UseStateWithArray from "./components/UseStateWithArray";
 // import ControlledForm from "./components/ControlledForm";
 // import LifeCyclesCDM from "./components/LifeCyclesCDM";
 // import LifeCyclesCDU from "./components/LifeCyclesCDU";
@@ -16,7 +17,8 @@ function App() {
       {/* <ControlledForm /> */}
       {/* <UncontrolledForm /> */}
       {/* <ControlledFormHooks /> */}
-      <UseStateWithArray />
+      {/* <UseStateWithArray /> */}
+      <UseStateWithObjects />
     </div>
   )
 }
